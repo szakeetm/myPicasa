4fec24de88d13a10b4609f5eeb33a5dd38133300
Marton Ady <marton.ady@cern.ch>
2026-04-08 10:24:16 +0200

    style: reduce font and square thumbnail tiles

--- frontend/src/components/MediaGrid.tsx
+++ frontend/src/components/MediaGrid.tsx
@@ -11,7 +11,7 @@ type MediaGridProps = {
   onSelect: (assetId: number) => void;
 };
 
-const ROW_HEIGHT = 260;
+const ROW_HEIGHT = 228;
 
 function columnCount(width: number) {
   if (width < 640) return 1;
